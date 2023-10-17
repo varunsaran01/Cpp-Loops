3dbe102d1347819495249f095bf5b640e944af49
varunsaran01 <148057866+varunsaran01@users.noreply.github.com>
2023-10-17 15:11:07 +0530

    Create for_loop_Hello_World.cpp

---
+++ for_loop_Hello_World.cpp
@@ -0,0 +1,29 @@
+/*PRN - 22070123130
+Print Hello World 10 Times Using For Loop*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        cout << i +1 << " Hello World" << endl;
+    }
+    
+    return 0;
+}
+/*
+Output:
+1 Hello World
+2 Hello World
+3 Hello World
+4 Hello World
+5 Hello World
+6 Hello World
+7 Hello World
+8 Hello World
+9 Hello World
+10 Hello World
+*/
