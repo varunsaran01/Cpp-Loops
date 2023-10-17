d9fc09845ae824d4f1d21f1f43184d877d791565
varunsaran01 <148057866+varunsaran01@users.noreply.github.com>
2023-10-17 15:10:13 +0530

    Create calculator_switch.cpp

---
+++ calculator_switch.cpp
@@ -0,0 +1,53 @@
+/*
+PRN - 22070123130
+
+C++ program to create calculator using
+switch statement
+
+*/
+
+# include <iostream>
+using namespace std;
+
+int main() {
+
+  char op;
+  float num1, num2;
+
+  cout << "Enter operator: +, -, *, /: ";
+  cin >> op;
+
+  cout << "Enter two operands: ";
+  cin >> num1 >> num2;
+
+  switch(op) {
+
+    case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      break;
+
+    case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      break;
+
+    case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      break;
+
+    case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      break;
+
+    default:
+      cout << "Error! operator is not correct";
+      break;
+  }
+
+  return 0;
+}
+/*
+Output:
+Enter operator: +, -, *, /: *
+Enter two operands: 56 98
+56 * 98 = 5488
+*/
